99dd6b9488e9a3bb7be3e5a04c079ca6ee559e5f
Musferr <syed.musfer@convexinteractive.com>
2026-04-16 16:13:26 +0500

    Update API and AGENT base URLs to port 9099

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://webrtc.convexinteractive.com:8099/';
-export const AGENT_BASE_URL = 'https://webrtc.convexinteractive.com:8099/';
+export const API_BASE_URL = 'https://webrtc.convexinteractive.com:9099/';
+export const AGENT_BASE_URL = 'https://webrtc.convexinteractive.com:9099/';
 export const d_BASE_URL = '/sssdf';
 // cmnt added

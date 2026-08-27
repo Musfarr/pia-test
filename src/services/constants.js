9990cab0951ba11cec9343e061a0be4a96205961
Musferr <syed.musfer@convexinteractive.com>
2026-08-27 16:42:40 +0500

    env2

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://c61d-103-197-46-226.ngrok-free.app';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://c61d-103-197-46-226.ngrok-free.app/api';
 export const AGENT_BASE_URL = import.meta.env.VITE_AGENT_BASE_URL || 'http://localhost:8000/api';

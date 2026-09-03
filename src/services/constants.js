f927c785c2aa9206591c6ac8c5b6a29d534d8b28
Musferr <syed.musfer@convexinteractive.com>
2026-09-03 19:51:09 +0500

    fx

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://51.254.14.130:9020/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://piawards.convexinteractive.com/api';
 export const AGENT_BASE_URL = import.meta.env.VITE_AGENT_BASE_URL || 'http://localhost:8000/api';
